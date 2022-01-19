bffabfebed7b63b96d7d5ae65d3667887bb55a6d
mitroph <i.am.mitroph@gmail.com>
2022-01-19 03:16:54 +0200

    Created first objects

--- js/script.js
+++ js/script.js
@@ -1,52 +1,21 @@
 'use strict';
 
-console.log('gg');
-
-let number = 5;
-const leftBorderWidth = 1;
-var name = 'Iban';
-
-number = 10;
-console.log(number);
-
-
-
-const obj = {
-    a:50
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies:{},
+    genres:{},
+    actors:{},
+    privat: false
 };
 
-obj.a = 10;
+const a = prompt('Last movie?', ''),
+      b = prompt('How many stars?', ''),
+      c = prompt('Last movie?'),
+      d = prompt('Stars?');
 
-console.log(obj);
-// leftBorderWidth = 10;        Error cause it's const
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB.movies);
 
-{
-    let result = 50;
-    console.log(result);
-}
-let apples = 2;
-let oranges = 3;
-
-
-
-console.log(`${apples} + ${oranges} = ${apples + oranges}`);
-
-let a = '';
-let b = "";
-let c = ``;
-
-const myObj = {
-    name: 'John',
-    age: 25,
-    isMarried: false
-};
-
-console.log(myObj.name);
-
-
-const answerName = prompt('Whats ur name?',"");
-const answerSecondName = prompt('Whats ur second name?',"");
-const answerAge = prompt('How old r u?',"");
-
-const answers = [answerName, answerSecondName, answerAge];
 
